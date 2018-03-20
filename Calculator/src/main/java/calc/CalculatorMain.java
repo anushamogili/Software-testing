a48a654f199091235dfb1fe81bde81a43c464b9c
sravanibejawada <shiru2597@gmail.com>
2018-03-20 23:43:13 +0100

    new

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -2,38 +2,9 @@ package calc;
 
 public class CalculatorMain {
 	public CalculatorMain(){
-		}
+		
+	}
 	public int add(int a, int b){
 		return a+b;
-	}
-	public int Sub(int a, int b) {
-		return a-b;
-	}
-	public int mul(int a, int b) {
-		return a*b;
-	}
-	public int div(int a, int b) {
-		if(b==0) {
-			return 0;
-		}
-		else {
-			return a/b;
-		}
-	}
-	public Object Square(int b) {
-		return b*b;
-	}
-	public Object fact(int a) {
-		
-		return a*(a-1);
-	}
-	public int sqrt(int a)
-	{
-		int b=(int)Math.sqrt(a);
-		System.out.println("sqrt is"+b);
-		return (int)Math.sqrt(a);
-}
-	public int remaind(int a, int c) {
-		return a%c;
 	}
 }
