ac0e7f7a96500d71e0bfd34dbd06d2784c65c0d5
anusha <mogilianusha127@gmail.com>
2018-03-20 21:29:53 +0100

    commit

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -1,11 +1,67 @@
 package calc;
 
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Scanner;
+
 public class CalculatorMain {
-	public CalculatorMain(){
-		
+
+	public int add(int a, int b) {
+		return a + b;
 	}
-//testing the commit and push
-	public int add(int a, int b){
-		return a+b;
+
+	public int sub(int x, int y) {
+		return x - y;
 	}
+
+	public int mul(int p, int q) {
+		return p * q;
+	}
+
+	public int div(int j, int k) throw Exception {
+		try {
+		int rem= j / k;
+		return rem;
+		}
+		catch(Exception e){
+			throw e;
+		}
+	}
+
+
+	public double sqr(double number) {
+		double squareRoot = Math.sqrt(number);
+		return Math.round(squareRoot);
+	}
+
+	public int mod(int num1, int num2) {
+		return num1 % num2;
+	}
+
+	public int area(int r) {
+		return 3.15 * this.sqaure(r) ;
+	}
+	public double sin(double val) {
+		return Math.sin(val);
+	}
+	public double cos(double val) {
+		return Math.cos(val);
+	}
+	public double tan(double val) {
+		return Math.tan(val);
+	}
+	public double sec(double val) {
+		return Math.sec(val);
+	}
+	public double log(double val) {
+		return Math.log(val);
+	}
+	public int power(int x,int y) {
+		return x^y;
+	}
+	public int sqaure(int val) {
+		return val*val;
+	}
+}
 }
