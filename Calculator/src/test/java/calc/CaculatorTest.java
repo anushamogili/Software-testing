dedf8f7b563142d1aada8ed5f910a1ebcb2bf997
Unknown <thkt17@student.bth.se>
2018-03-21 00:07:07 +0100

    karthik

--- Calculator/src/test/java/calc/CaculatorTest.java
+++ Calculator/src/test/java/calc/CaculatorTest.java
@@ -13,17 +13,17 @@ public class CaculatorTest {
 		calc = new CalculatorMain();
 	}
 	
-	@Test
+       @Test
 	public void testAdd() {
 		assertEquals(5, calc.add(3,2));
-	}
+       }
 	@Test
 	public void testSub() {
-		assertEquals(1, calc.sub(3,2));
+		assertEquals(1, calc.Sub(3,2));
 	}
 	@Test
 	public void testMul() {
-		assertEquals(6, calc.mul(3,2));
+	assertEquals(6, calc.mul(3,2));
 	}
 	@Test
 	public void testDiv() {
@@ -36,5 +36,21 @@ public class CaculatorTest {
 	@Test
 	public void testPresedence() {
 		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
+		}
+	@Test
+	public void testSquare() {
+		assertEquals(4, calc.Square(2));
 	}
+       @Test
+   	public void testfact() {
+   		assertEquals(2, calc.fact(2));
+          }
+       @Test
+   	public void testremaind() {
+   		assertEquals(1, calc.remaind(7,3));
+          }
+       @Test
+    public void testIntegrate() {
+    	   assertEquals (4, calc.Sub(calc.add(3,3), calc.div(4, 2)));
+       }
 }
