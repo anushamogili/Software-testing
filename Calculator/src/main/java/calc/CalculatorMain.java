0784e86a5daa81e3f929af4eeda7ef66cd7f65a2
sravanibejawada <shiru2597@gmail.com>
2018-03-22 11:43:18 +0100

    done by sravani

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -8,6 +8,18 @@ public class CalculatorMain {
 	}
 	public int Sub(int a, int b) {
 		return a-b;
+        
+	}
+        public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		else {
+			return a/b;
+		}
 	}
 	
 }
